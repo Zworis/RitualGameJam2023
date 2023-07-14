53317f62ae01bb6119d4aa731d4a73864ff1703f
Kai <55585019+ThedragonZkai@users.noreply.github.com>
2023-07-14 11:47:29 +0200

    changes to chcon

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,7 +6,14 @@ public class PlayerController : MonoBehaviour
 {
 	public float speed;
 	public float drag;
+	public float xSensitivity;
+	public float ySensitivity;
+
+
+
 	private Rigidbody rb;
+	private GameObject cam;
+	private float yValue;
 
 
 
@@ -15,13 +22,39 @@ public class PlayerController : MonoBehaviour
 	void Start()
     {
 		rb = GetComponent<Rigidbody>();
-		
+		LockCursor(true);
+		cam = Camera.main.gameObject;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(new Vector3(Input.GetAxisRaw("Horizontal")* speed, 0, Input.GetAxisRaw("Horizontal") * speed), ForceMode.Impulse);
+        rb.AddRelativeForce(new Vector3(Input.GetAxisRaw("Horizontal")* speed, 0, Input.GetAxisRaw("Vertical") * speed), ForceMode.Impulse);
 		rb.AddForce(new Vector3(rb.velocity.x  * -drag, 0, rb.velocity.z  * -drag), ForceMode.Impulse);
-    }
+		transform.Rotate(new Vector3(0,xSensitivity * Time.timeScale * Input.GetAxis("Mouse X"),0));
+        cam.transform.localRotation = Quaternion.Euler(yValue, 0, 0);
+        yValue -= Input.GetAxis("Mouse Y") * ySensitivity * Time.timeScale;        
+        if (yValue > 90)
+        {
+            yValue = 90;
+        }
+        else if (yValue < -90)
+        {
+            yValue = -90;
+        }
+
+	}
+
+
+
+	void LockCursor(bool doLock) {
+		if (doLock == true) {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+		}
+		else {
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
 }
