d827f4f2bd9c89af15a211355f3abc65eca91aa0
Kai <55585019+ThedragonZkai@users.noreply.github.com>
2023-07-14 11:25:46 +0200

    added charactercontroller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	public 
+	public float speed;
+	public float drag;
+	private Rigidbody rb;
 
 
 
-    // Start is called before the first frame update
-    void Start()
+
+	// Start is called before the first frame update
+	void Start()
     {
-        
-    }
+		rb = GetComponent<Rigidbody>();
+		
+	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(new Vector3(Input.GetAxisRaw("Horizontal")* speed, 0, Input.GetAxisRaw("Horizontal") * speed), ForceMode.Impulse);
+		rb.AddForce(new Vector3(rb.velocity.x  * -drag, 0, rb.velocity.z  * -drag), ForceMode.Impulse);
     }
 }
